feac995b31c2c511839014c7bcc55d79e634e9dd
Diksha Sharma <drawdi16@gmail.com>
2025-10-30 01:14:29 +0530

    Page1 comp

--- page1.js
+++ page1.js
@@ -13,3 +13,30 @@ getquote(api_url);
 setInterval(() => {
     getquote(api_url);
 }, 86400000);
+
+let savedata = null;
+let savestreak = null;
+let data = document.getElementById("data");
+let streak = document.getElementById("streak");
+let middle = document.getElementById("middle");
+savedata=data;
+savestreak=streak;
+
+function hobbies(){
+    data.remove();
+    streak.remove();
+    middle.appendChild(streak);
+    streak.style.animation = "moveright 0.5s linear forwards"
+    middle.appendChild(data);
+    data.style.animation = "moveleft 0.5s linear forwards"
+}
+
+function work(){
+    data.remove();
+    streak.remove();
+    middle.appendChild(data);
+    data.style.animation = "moveright 0.5s linear forwards"
+    middle.appendChild(streak);
+    streak.style.animation = "moveleft 0.5s linear forwards"
+
+}
